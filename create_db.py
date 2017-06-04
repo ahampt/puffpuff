a7844843b167fbe879119bcb327cdacacb02ef11
ahampt <alhampt@gmail.com>
2017-06-03 19:10:41 -0500

    Create DB in same folder as script

--- create_db.py
+++ create_db.py
@@ -1,7 +1,9 @@
 import sqlite3
+import os
+dir = os.path.dirname(__file__)
 
 # Connecting to the database file
-conn = sqlite3.connect('puff_puff_db.sqlite')
+conn = sqlite3.connect(os.path.join(dir, 'puff_puff_db.sqlite'))
 c = conn.cursor()
 
 c.execute('CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, SortIndex INTEGER UNIQUE NOT NULL, IsCurrentlyPuffing INTEGER DEFAULT 0 NOT NULL, CountPaidEarly INTEGER DEFAULT 0 NOT NULL)')
